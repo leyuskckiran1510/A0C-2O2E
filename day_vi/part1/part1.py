ad8fc8a028c4c60c4b7d836a01409a06914b86e8
leyuskckiran1510 <kdhakal1510@gmail.com>
2023-12-10 14:18:56 +0545

    cleanups

---
+++ day_vi/part1/part1.py
@@ -0,0 +1,33 @@
+from functools import reduce
+from sys import argv
+
+
+def main():
+    times = []
+    distances = []
+    each = []
+    with open(argv[1], "r") as fp:
+        times = list(map(int, filter(lambda x: x, fp.readline().split(":")[-1].strip().split(" "))))
+        distances = list(map(int, filter(lambda x: x, fp.readline().split(":")[-1].strip().split(" "))))
+    for x, y in zip(times, distances):
+        left = 1
+        right = x - left
+        count = 0
+        while left <= x:
+            if left * (x - left) > y:
+                count += 1
+            left += 1
+            # right -= 1
+        # if count % 2:
+        #     count = (count - 1) * 2 + 1
+        # else:
+        #     count = count * 2
+        each.append(count)
+    print(each, reduce(lambda x, y: x * y, each))
+
+
+if __name__ == "__main__":
+    if len(argv) < 2:
+        print("Provide A input File")
+    else:
+        main()
